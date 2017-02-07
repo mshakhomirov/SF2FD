1af10119042e5623da82f7bc92df34c967c50d80
mshakhomirov <mshakhomirov@gmail.com>
2017-02-07 07:15:45 +0000

    Update SimpleObject.php

--- SimpleObject.php
+++ SimpleObject.php
@@ -1,19 +1,43 @@
-<?php
-class SimpleObject {
-
-    function __get($key)
-    {
-        return (isset($this->{$key})) ? $this->{$key} : NULL;
-    }
-    
-    public function get($key)
-    {
-        return (isset($this->{$key})) ? $this->{$key} : NULL;
-    }
-
-
-    public function set($key, $value)
-    {
-        $this->{$key} = $value;
-    }
+<?php
+
+class SimpleObject {
+
+<?php
+
+class SimpleObject {
+
+
+
+    function __get($key)
+
+    {
+
+        return (isset($this->{$key})) ? $this->{$key} : NULL;
+
+    }
+
+    
+
+
+    public function get($key)
+
+    {
+
+        return (isset($this->{$key})) ? $this->{$key} : NULL;
+
+    }
+
+
+
+
+
+    public function set($key, $value)
+
+    {
+
+        $this->{$key} = $value;
+
+    }
+
 }
+
